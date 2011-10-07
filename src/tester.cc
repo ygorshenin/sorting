2dcbe3c373053f43ff92832566f47828f2daa98c
ygorshenin <ygorshenin@google.com>
2011-10-07 13:19:21 +0400

    s/sort/sorters

--- src/tester.cc
+++ src/tester.cc
@@ -6,17 +6,17 @@
 #include "base/timer.h"
 #include "generators/generator_interface.h"
 #include "generators/random_generator.h"
-#include "sort/sorter_interface.h"
-#include "sort/stl_sorters.h"
+#include "sorters/sorter_interface.h"
+#include "sorters/stl_sorters.h"
 
 using namespace base;
 using namespace generators;
-using namespace sort;
+using namespace sorters;
 
 
 template<typename T, template <typename> class Comparer>
 double TestSortingAlgorithm(size_t size, T *data,
-			    sort::SorterInterface<T, Comparer> *sorter) {
+			    SorterInterface<T, Comparer> *sorter) {
   base::Timer timer;
   timer.Start();
   sorter->Sort(size, data);
@@ -29,9 +29,16 @@ double TestSortingAlgorithm(size_t size, T *data,
 }
 
 int main(int argc, char **argv) {
-  SorterInterface<int> *sorter = new StlBasicSorter<int>();
+  SorterInterface<int> *sorter = new StlInplacePartitionSorter<int>();
   GeneratorInterace<int> *generator = new RandomGenerator<int>();
-  delete sorter;
+
+  const size_t kSize = 20000000;
+  int *data = new int [kSize];
+  generator->Generate(kSize, data);
+  printf("Elapsed time: %.4lf\n", TestSortingAlgorithm(kSize, data, sorter));
+
+  delete [] data;
   delete generator;
+  delete sorter;
   return 0;
 }
