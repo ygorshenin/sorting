eb1ebe4afe87c0e3c79dfa5b15ff3d75a65ea763
ygorshenin <ygorshenin@google.com>
2011-10-14 02:19:39 +0400

    Added insertion sorter algorithm.

---
+++ src/sorters/insertion_sorter.h
@@ -0,0 +1,38 @@
+#ifndef SORTERS_INSERTION_SORTER_H
+#define SORTERS_INSERTION_SORTER_H
+
+#include <functional>
+
+#include "base/macros.h"
+#include "sorters/sorter_interface.h"
+
+
+namespace sorters {
+
+template<typename T, template <typename> class Comparer = std::less>
+class InsertionSorter: public SorterInterface<T, Comparer> {
+ public:
+   InsertionSorter() {}
+
+   virtual void Sort(size_t size, T *objects) {
+     Comparer<T> comparer;
+
+     size_t i = 1, j;
+     while (i < size) {
+       T current(objects[i]);
+       j = i;
+
+       while (j > 0 && !comparer(objects[j - 1], current)) {
+	 objects[j] = objects[j - 1];
+	 --j;
+       }
+       objects[j] = current;
+
+       ++i;
+     }
+   }
+}; // class InsertionSorter
+
+}  // namespace sorters
+
+#endif // #ifndef SORTERS_INSERTION_SORTER_H
