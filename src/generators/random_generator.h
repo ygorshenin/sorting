a2a6bac59b67daf585cd491ac2f317493cc24cee
ygorshenin <ygorshenin@google.com>
2011-10-16 15:35:53 +0400

    Added random generator specialization for meta-vectors.

--- src/generators/random_generator.h
+++ src/generators/random_generator.h
@@ -4,10 +4,14 @@
 #include <cstdlib>
 
 #include "base/macros.h"
+#include "base/vector.h"
 #include "generators/generator_interface.h"
 
 
 namespace generators {
+
+using base::Vector;
+
 
 template<typename T>
 class RandomGenerator: public GeneratorInterace<T> {
@@ -33,6 +37,23 @@ class RandomGenerator<int>: public GeneratorInterace<int> {
  private:
    DISABLE_EVIL_CONSTRUCTORS(RandomGenerator);
 }; // class RandomGenerator
+
+template<size_t N>
+class RandomGenerator<Vector<N, int> >:
+  public GeneratorInterace<Vector<N, int> > {
+ public:
+  RandomGenerator() {}
+
+  virtual void Generate(size_t size, Vector<N, int> *buffer) {
+    for (size_t i = 0; i < size; ++i)
+      for (size_t j = 0; j < N; ++j)
+	buffer[i][j] = rand();
+  }
+
+ private:
+  DISABLE_EVIL_CONSTRUCTORS(RandomGenerator);
+}; // class RandomGenerator
+
 
 }  // namespace generators
 
