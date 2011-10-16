3333a4907258b64cd320b4b459d100d310794d51
ygorshenin <ygorshenin@google.com>
2011-10-16 15:36:42 +0400

    Added meta-vector support for tester.

--- src/tester.cc
+++ src/tester.cc
@@ -1,7 +1,8 @@
 #include <assert.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <sys/stat.h>
-
+#include <time.h>
 
 #include <algorithm>
 #include <fstream>
@@ -10,15 +11,17 @@
 #include <string>
 #include <vector>
 
+#include <gflags/gflags.h>
+
 #include "base/macros.h"
 #include "base/timer.h"
+#include "base/vector.h"
 #include "generators/generator_interface.h"
 #include "generators/random_generator.h"
+#include "sorters/insertion_sorter.h"
 #include "sorters/sorter_interface.h"
 #include "sorters/stl_sorters.h"
-#include "sorters/insertion_sorter.h"
 
-#include <gflags/gflags.h>
 
 using namespace std;
 
@@ -27,12 +30,20 @@ using namespace generators;
 using namespace sorters;
 
 
-DEFINE_bool(use_insertion_sort, false, "use insertion sort in tests");
-DEFINE_int32(max_power, 24, "maximum power of two that will be used as test size");
-DEFINE_string(out_dir, "out", "output directory for statistics");
+const int kMaxNumDimensions = 6;
+
+DEFINE_bool(use_insertion_sort, false, "Use insertion sort in tests.");
+DEFINE_int32(max_power, 24, "Maximum power of two that will be used \
+                             as test size.");
+DEFINE_int32(seed, 0, "seed for random generator. If zero, time is used \
+                       as seed.");
+DEFINE_int32(num_dimensions, 0, "Number of vector dimensions, from 0 to 5, \
+                                 inclusively. \
+                                 If zero, plain ints will be sorted.");
+DEFINE_string(out_dir, "out", "Output directory for statistics.");
 
 
-template<typename T, template <typename> class Comparer>
+template<typename T, typename Comparer>
 double TestSortingAlgorithm(size_t size, T *data,
 			    SorterInterface<T, Comparer> *sorter) {
   base::Timer timer;
@@ -41,7 +52,7 @@ double TestSortingAlgorithm(size_t size, T *data,
   sorter->Sort(size, data);
   timer.Stop();
 
-  Comparer<T> comparer;
+  Comparer comparer;
   for (size_t i = 0; i + 1 < size; ++i)
     assert(!comparer(data[i + 1], data[i]));
 
@@ -51,7 +62,7 @@ double TestSortingAlgorithm(size_t size, T *data,
   return elapsed_time;
 }
 
-template<typename T, template <typename> class Comparer>
+template<typename T, typename Comparer>
 void TwoPowerTesting(size_t max_power,
 		     GeneratorInterace<T> *generator,
 		     vector<SorterInterface<T, Comparer> *> &sorters,
@@ -102,43 +113,27 @@ void DumpStatistic(const string &out_dir,
   }
 }
 
+template<typename T, typename Comparer>
+void TestSortingAlgorithms() {
+  GeneratorInterace<T> *generator = new RandomGenerator<T>();
 
-int main(int argc, char **argv) {
-  string usage = "This program measures performance of different sorting algorithms\n";
-  usage += "Sample usage: ";
-  usage += argv[0];
-  usage += " --max_power=20\n";
-
-  google::SetUsageMessage(usage);
-  google::ParseCommandLineFlags(&argc, &argv, true);
-
-  assert(FLAGS_out_dir != "");
-  assert(FLAGS_max_power >= 0);
-
-  if (mkdir(FLAGS_out_dir.c_str(), 0777) != 0 && errno != EEXIST) {
-    perror("Can't create output directory");
-    return 1;
-  }
-
-  GeneratorInterace<int> *generator = new RandomGenerator<int>();
-
-  vector<SorterInterface<int> *> sorters;
+  vector<SorterInterface<T, Comparer> *> sorters;
   vector<string> sorters_names;
 
-  sorters.push_back(new StlBasicSorter<int>());
+  sorters.push_back(new StlBasicSorter<T, Comparer>());
   sorters_names.push_back("stl_basic_sorter");
 
-  sorters.push_back(new StlHeapSorter<int>());
+  sorters.push_back(new StlHeapSorter<T, Comparer>());
   sorters_names.push_back("stl_heap_sorter");
 
-  sorters.push_back(new StlPartitionSorter<int>());
+  sorters.push_back(new StlPartitionSorter<T, Comparer>());
   sorters_names.push_back("stl_partition_sorter");
 
-  sorters.push_back(new StlInplacePartitionSorter<int>());
+  sorters.push_back(new StlInplacePartitionSorter<T, Comparer>());
   sorters_names.push_back("stl_inplace_partition_sorter");
 
   if (FLAGS_use_insertion_sort) {
-    sorters.push_back(new InsertionSorter<int>());
+    sorters.push_back(new InsertionSorter<T, Comparer>());
     sorters_names.push_back("insertion_sorter");
   }
 
@@ -152,5 +147,46 @@ int main(int argc, char **argv) {
   delete generator;
   for (size_t cur_sorter = 0; cur_sorter < sorters.size(); ++cur_sorter)
     delete sorters[cur_sorter];
+}
+
+
+int main(int argc, char **argv) {
+  string usage = "This program measures performance of \
+                  different sorting algorithms\n";
+  usage += "Sample usage: ";
+  usage += argv[0];
+  usage += " --max_power=20\n";
+
+  google::SetUsageMessage(usage);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  assert(FLAGS_out_dir != "");
+  assert(FLAGS_max_power >= 0);
+
+  if (FLAGS_seed == 0)
+    srand(time(NULL));
+  else
+    srand(FLAGS_seed);
+
+  if (mkdir(FLAGS_out_dir.c_str(), 0777) != 0 && errno != EEXIST) {
+    perror("Can't create output directory");
+    return 1;
+  }
+
+  assert(FLAGS_num_dimensions >= 0);
+  assert(FLAGS_num_dimensions < kMaxNumDimensions);
+
+  if (FLAGS_num_dimensions == 0)
+    TestSortingAlgorithms<int, less<int> >();
+  else if (FLAGS_num_dimensions == 1)
+    TestSortingAlgorithms<Vector<1, int>, VectorComparer<1, int> >();
+  else if (FLAGS_num_dimensions == 2)
+    TestSortingAlgorithms<Vector<2, int>, VectorComparer<2, int> >();
+  else if (FLAGS_num_dimensions == 3)
+    TestSortingAlgorithms<Vector<3, int>, VectorComparer<3, int> >();
+  else if (FLAGS_num_dimensions == 4)
+    TestSortingAlgorithms<Vector<4, int>, VectorComparer<4, int> >();
+  else if (FLAGS_num_dimensions == 5)
+    TestSortingAlgorithms<Vector<5, int>, VectorComparer<5, int> >();
   return 0;
 }
